e794d2d07f75034675f303c814a726e68bd89509
HugoCapitan <hmoravilez@gmail.com>
2018-05-23 12:43:40 -0500

    hasmap for cards

--- src/uno/Player.java
+++ src/uno/Player.java
@@ -1,24 +1,34 @@
 package uno;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Player
  */
 public class Player {
+  private static String qwerty = "qwertyuiopasdfghjklzxcvbnm";
 
   private String name;
-  private List<Card> cards;
+  private Map<String, Card> cards = new HashMap<String, Card>();
 
   // public Player(String name) {
   public Player(String name, List<Card> cards) {
     this.name = name;
-    this.cards = cards;
+    this.addCards(cards);
+  }
+
+  public void addCards(List<Card> cards) {
+    for (int i = 0; i < cards.size(); i++) {
+      Card cardToAdd = cards.get(i);
+      this.cards.put(qwerty.substring(i, i + 1), cardToAdd);
+    }
   }
 
   public String getName() { return this.name; }
 
-  public List<Card> getCardS() { return this.cards; }
+  public Map<String, Card> getCardS() { return this.cards; }
 
 }

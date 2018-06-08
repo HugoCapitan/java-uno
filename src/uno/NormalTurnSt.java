8ae21afdfb875b8b9b627dedc704bb0568283fb2
HugoCapitan <hmoravilez@gmail.com>
2018-06-08 12:32:59 -0500

    playCard moved to turnStates

--- src/uno/NormalTurnSt.java
+++ src/uno/NormalTurnSt.java
@@ -1,6 +1,7 @@
 package uno;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * NormalTurnSt
@@ -10,6 +11,43 @@ public class NormalTurnSt implements TurnSt {
 
   public NormalTurnSt(Uno uno) {
     this.uno = uno;
+  }
+
+  @Override
+  public void playCard(Player turnPlayer, String selectionChar) {
+    int turnsCounter = uno.getTurnsCounter();
+    Card selectedCard = turnPlayer.getCards().get(selectionChar);
+    LinkedList<Card> deck = uno.getDeck();
+    LinkedList<Card> stack = uno.getStack();
+    ListIterator playersIterator = uno.getPlayersIterator();
+    
+    if (selectedCard != null) {
+      if (turnsCounter == 1 || stack.getFirst().isCompatible(selectedCard)) {
+        stack.addFirst( turnPlayer.pickCard(selectionChar) );
+
+        if (selectedCard.getNumber().equals("Flip")) {
+          if (uno.retrievePlayerSt instanceof NextPlayerSt) {
+            uno.retrievePlayerSt = uno.prevPlayerSt;
+            playersIterator.previous();
+          } else {
+            uno.retrievePlayerSt = uno.nextPlayerSt;
+            playersIterator.next();
+          }
+        }
+
+        if (selectedCard.getNumber().equals("Block")) // Pass the next player
+          uno.retrievePlayer();
+
+        uno.nextTurn();
+      } else {
+        // TODO: Display "this card can't be played" message
+        uno.turn(turnPlayer);
+      }
+    } else {
+      // TODO: Display "this card doesn't exists" message
+      uno.turn(turnPlayer);
+    }
+    
   }
 
   @Override
--- src/uno/TurnSt.java
+++ src/uno/TurnSt.java
@@ -9,4 +9,5 @@ public interface TurnSt {
   Scanner in = new Scanner(System.in);
 
   public void turn(Player turnPlayer);
+  public void playCard(Player turnPlayer, String selectionChar);
 }

5e0f916cb3ea0df30121e39992537f0cca87d1e7
HugoCapitan <hmoravilez@gmail.com>
2018-06-08 12:26:07 -0500

    Post powers implementations declarations

---
+++ src/uno/PostDrawTurnSt.java
@@ -0,0 +1,18 @@
+package uno;
+
+/**
+ * PostDrawTurnSt
+ */
+public class PostDrawTurnSt implements TurnSt{
+  Uno uno;
+
+  public PostDrawTurnSt(Uno uno) {
+    this.uno = uno;
+  }
+
+  @Override
+  public void turn(Player turnPlayer) {
+    
+  }
+
+}
---
+++ src/uno/PostWildTurnSt.java
@@ -0,0 +1,17 @@
+package uno;
+
+/**
+ * PostWildTurnSt
+ */
+public class PostWildTurnSt implements TurnSt {
+  Uno uno;
+
+  public PostWildTurnSt (Uno uno) {
+    this.uno = uno;
+  }
+  
+  @Override
+  public void turn(Player turnPlayer) {
+    
+  }
+}

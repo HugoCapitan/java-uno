7c1e3c59f0a61d28bd53e978fc0ed747eb09eaeb
HugoCapitan <hmoravilez@gmail.com>
2018-06-11 13:46:57 -0500

    cleaning code, unused vars and reusing functionality

--- src/uno/PostWildTurnSt.java
+++ src/uno/PostWildTurnSt.java
@@ -15,44 +15,18 @@ public class PostWildTurnSt implements TurnSt {
 
   @Override
   public void playCard(Player turnPlayer, String selectionChar) {
-    int turnsCounter = uno.getTurnsCounter();
     String wildColor = uno.getWildColor();
     Card selectedCard = turnPlayer.getCards().get(selectionChar);
-    LinkedList<Card> deck = uno.getDeck();
     LinkedList<Card> stack = uno.getStack();
-    ListIterator playersIterator = uno.getPlayersIterator();
     
     if (selectedCard != null && selectedCard.getColor().equals(wildColor) || selectedCard.getColor().equals("Wild")) {
       stack.addFirst( turnPlayer.pickCard(selectionChar) );
       uno.turnSt = uno.normalTurnSt;
 
-      switch(selectedCard.getNumber()) {
-        case "Flip":
-          if (uno.retrievePlayerSt instanceof NextPlayerSt) {
-            uno.retrievePlayerSt = uno.prevPlayerSt;
-            playersIterator.previous();
-          } else {
-            uno.retrievePlayerSt = uno.nextPlayerSt;
-            playersIterator.next();
-          }
-          break;
-        case "Block":
-          uno.retrievePlayer();
-          break;
-        case "+2":
-        case "+4":
-        case "Wild":
-          uno.setWildColor(Printer.askForWildColor());
-          uno.turnSt = uno.postWildTurnSt;
-          break;
-        default:
-          break;
-      }
+      uno.reactToCard(selectedCard);
 
       uno.nextTurn();
-      
     } else {
-      // TODO: Display "this card doesn't exists" message
       uno.turn(turnPlayer);
     }
         
@@ -61,22 +35,18 @@ public class PostWildTurnSt implements TurnSt {
   @Override
   public void turn(Player turnPlayer) {
     String selectionChar;
-    int turnsCounter = uno.getTurnsCounter();
     LinkedList<Card> deck = uno.getDeck();
     LinkedList<Card> stack = uno.getStack();
 
     if (deck.size() == 0) 
       uno.refillDeck();
 
-    if (turnsCounter == 1)
-      Printer.printFirstTurn(turnPlayer);
-    else   
-      Printer.printTurn(turnPlayer, stack.getFirst());
+    Printer.printTurn(turnPlayer, stack.getFirst());
 
     // Reading user input
     selectionChar = in.next();
 
-    if (selectionChar.equals("pass") && turnsCounter > 1) 
+    if (selectionChar.equals("pass")) 
       uno.pass();
     else if (selectionChar.equals("eat"))
       uno.eatCard(turnPlayer);
